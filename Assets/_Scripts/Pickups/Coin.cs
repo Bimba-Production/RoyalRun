5de25bab82a47e5fc5cb7fad78923bda4482e2a6
dakamakat <gazon-ozon@yandex.ru>
2025-01-02 10:40:42 +0100

    update onpickup's

--- Assets/_Scripts/Pickups/Coin.cs
+++ Assets/_Scripts/Pickups/Coin.cs
@@ -1,9 +1,12 @@
-namespace Assets._Scripts
+using _Scripts.UI;
+
+namespace _Scripts.Pickups
 {
     public class Coin : Pickup
     {
         public override void OnPickup()
         {
+            ScoreDisplay.Instance.IncreaseScore(1);
             Destroy(gameObject);
         }
     }
--- Assets/_Scripts/Pickups/Crown.cs
+++ Assets/_Scripts/Pickups/Crown.cs
@@ -1,9 +1,12 @@
-namespace Assets._Scripts
+using _Scripts.Level;
+
+namespace _Scripts.Pickups
 {
     public class Crown : Pickup
     {
         public override void OnPickup()
         {
+            LevelGenerator.Instance.SlowDownTheLevel();
             Destroy(gameObject);
         }
     }
